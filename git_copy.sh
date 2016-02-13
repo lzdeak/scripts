e195a202d377be00a90b90bea38b685f95458116
Lajos Deak <ldeak@yahoo-inc.com>
2016-02-13 00:39:56 +0000

    fix filtering for dst_rel

--- git_copy.sh
+++ git_copy.sh
@@ -19,8 +19,11 @@ src_rel=$(echo $src_abs | sed "s#$src_root/##g")
 outfile=$(basename $dst_rel)_$(date +%s).patch
 git format-patch --stdout \
 $(git log $(basename $src_abs) | grep ^commit|tail -1|awk '{print $2}')^..HEAD $(basename $src_abs) |\
-awk 'BEGIN{ena=1} /^diff /{ if ( $0 ~ /'$(echo "$dst_rel" | sed 's#/#\\/#g')'$/ ) ena=1; else ena=0 } { if (ena==1) print; }' |\
 sed "s#$src_rel#$dst_rel#g" |\
+awk 'BEGIN{ena=1} /^diff /{ if ( $0 ~ /'$(echo "$dst_rel" | sed 's#/#\\/#g')'$/ ) ena=1; else ena=0 } { if (ena==1) print; }' |\
 (popd > /dev/null; cat > $outfile)
 echo $src_root///$src_rel " -->  $dst_root///$dst_rel"
 echo git am $outfile
+
+
+
